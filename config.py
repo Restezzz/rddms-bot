a9445c0ea116c9c6cad44f0bb10deba60f2649fc
Llyesviel <viktoriaelajder@gmail.com>
2025-04-19 23:02:05 +0500

    update

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7994914232:AAFlg-YfYKw9QQHY3jyZ22V14rXPwlGiqyQ")
 
 # Настройки OpenRouter API
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "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")
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY", "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")
 OPENROUTER_API_URL = "https://openrouter.ai/api/v1/chat/completions"
-OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL", "meta-llama/llama-4-maverick:free") 
+OPENROUTER_MODEL = os.getenv("OPENROUTER_MODEL", "deepseek/deepseek-r1:free") 
